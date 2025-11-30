2d2757ad19c53c470e42d45bd37097ad97e4006e
Sar0ch <Sar0ch@users.noreply.github.com>
2025-11-30 08:10:55 +0700

    Add week13

--- Week13/forpractice.js
+++ Week13/forpractice.js
@@ -1,1 +1,28 @@
+//ฝึกทำด้วย Integrated Project รอบนี้ทำส่วนรับหน้าที่ทำในส่วนของ Backend โค้ดที่ได้ฝึกของ Frontend เลยมีน้อย
+// Load Declared Plan
+async function loadDeclaredPlan(statusMsg, declareBtn) {
 
+  const studentId = keycloakInstance.tokenParsed?.preferred_username;
+
+  try {
+    const response = await fetch(`${BASE_URL}/students/${studentId}/declared-plan`);
+
+    if (response.status === 404) {
+      statusMsg.textContent = "Declaration Status: Not Declared";
+      declareBtn.style.display = "inline-block";
+      declareBtn.disabled = true;
+      return;
+    }
+    const data = await response.json();
+    declareBtn.style.display = "none";
+
+    const updatedAt = new Date(data.updatedAt);
+    const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const formatted = updatedAt.toLocaleString("en-GB", { timeZone: userTimeZone });
+
+    statusMsg.textContent = `Declaration Status: Declared ${data.planCode} - ${data.planNameEng} on ${formatted} (${userTimeZone})`;
+
+  } catch (error) {
+    console.error(" Error loading declared plan:", error);
+  }
+}
---
+++ Week13/inclassweek13.js
@@ -0,0 +1,9 @@
+// ⏺️ Vite + JSON Server
+//     └── Frontend รันด้วย npm run dev
+//     └── Backend JSON Server รันด้วย npm run backend (localhost:5001)
+
+// ⏺️ async/await
+//     await ต้องอยู่ในฟังก์ชันที่เป็น async
+//     fetch ถ้า URL ผิด จะไม่ได้ JSON แต่ได้ HTML => ต้องเช็คเสมอว่า res.ok รึป่าว
+//     เวลา fetch fail  เราควร throw error แล้ว catch ให้ user
+
