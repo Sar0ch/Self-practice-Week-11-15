d8ccfc0bb059d8312af08b150f01f457704ab181
Sar0ch <Sar0ch@users.noreply.github.com>
2025-11-30 07:20:33 +0700

    Week12

--- Week13/quotes-project/js/myLib/fetchUtils.js
+++ Week13/quotes-project/js/myLib/fetchUtils.js
@@ -1,14 +1,28 @@
 //CRUD on any items
 //GET
 async function getItems(url) {
+  let message = ""
   try {
     const res = await fetch(url) //fetch returns respnse object
     console.log(res)
+    if (!res.ok) {
+      switch (res.status) {
+        case 404:
+          message = "404 - Item not found"
+          break
+        case 409:
+          message = "409 - Conflict"
+          break
+        default:
+          message = "Fail to get item, please try again"
+      }
+      throw new Error(message)
+    }
     const data = await res.json() //json() converts json string to JavaScript object
     console.log(data)
     return data
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error.message)
   }
 }
 export { getItems }
